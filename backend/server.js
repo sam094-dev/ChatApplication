0366d2aebdb29b76901acf0ccfe7cac5420b65b4
sam094-dev <116539109+sam094-dev@users.noreply.github.com>
2023-11-26 09:56:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-2vjc.onrender.com",
     // credentials: true,
   },
 });
